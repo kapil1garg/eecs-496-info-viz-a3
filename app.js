fb6e794c1d6521c63510af3ac6229dd61688ac2b
Kapil Garg <kapil1garg@gmail.com>
2019-05-13 18:10:55 -0500

    heroku setup

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-let express = require('express');
-let app = express();
-let path = require('path');
+// packages to run server
+const path = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
 
+// get routes and start setup of app
+const app = express();
+
+// app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/data'));
 
+// setup default route
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + 'index.html'));
 });
 
-let port = 3000;
-app.listen(3000);
-console.log(`Listening on port ${ port }`)
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
